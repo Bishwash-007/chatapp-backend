8d6bc363f12b39f392c89bed52fe6d0e12463b8f
Bishwash <drewwbishwash@gmail.com>
2025-07-14 00:12:26 +0545

    clean up

--- src/app.js
+++ src/app.js
@@ -3,15 +3,17 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 import authRouter from "./routes/auth.routes.js";
+import messageRouter from "./routes/message.routes.js";
 
 const app = express();
-app.use(cors({origin: process.env.CORS_ORIGIN,credentials: true,}));
-app.use(express.json({limit: "16kb",}));
-app.use(express.urlencoded({extended: true,limit: "16kb",}));
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
 //routes
-app.use("/api/v1/auth",authRouter)
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/message", messageRouter);
 
 export { app };
