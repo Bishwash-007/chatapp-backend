a28a589b85a5171a8c37e34844c196f8651de2b2
Bishwash <drewwbishwash@gmail.com>
2025-07-17 16:35:34 +0545

    fixed

--- src/middlewares/multer.middlewares.js
+++ src/middlewares/multer.middlewares.js
@@ -1,13 +1,19 @@
 import multer from "multer";
 import path from "path";
 import fs from "fs";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const uploadDir = path.join(__dirname, "../public/uploads/");
+    const uploadDir = path.join(__dirname, "../public");
+
     if (!fs.existsSync(uploadDir)) {
       fs.mkdirSync(uploadDir, { recursive: true });
     }
+
     cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
